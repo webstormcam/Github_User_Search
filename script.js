aea9cbde9f8edfbf7625852044134bfdc1caac76
Cameron Chandler <webstormcam@gmail.com>
2022-04-05 05:29:34 -0400

    Pulled in the Octocat image

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let user ='octocat'
-let url = `https://api.github.com/users/${user}`;
-
+let url = `https://api.github.com/users/octocat`;
+let user_icon = document.getElementById('user_icon');
 
 
 
 fetch(url)
 .then(res => res.json())
 .then((out) => {
+user_icon.src= out.avatar_url
 console.log(out)
 })
 .catch(err => { throw err });
