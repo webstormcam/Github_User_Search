ad8a9fbe4e88a596ac2b683108c7cba2b3b09790
Cameron Chandler <webstormcam@gmail.com>
2022-04-05 05:50:42 -0400

    Probably going to restrict expansion

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let user ='octocat'
 let url = `https://api.github.com/users/octocat`;
 let user_icon = document.getElementById('user_icon');
+let user_name = document.getElementById('username');
+let user_AT_name = document.getElementById('user@name');
 
 
 
 fetch(url)
 .then(res => res.json())
 .then((out) => {
-user_icon.src= out.avatar_url
+user_icon.src= out.avatar_url;
+user_name.innerHTML = out.name;
+user_AT_name.innerHTML += out.login;
 console.log(out)
 })
 .catch(err => { throw err });
