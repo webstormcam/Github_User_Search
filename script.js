f301e5dd5e0b456e0f23e372da9736f21810cab8
Cameron Chandler <webstormcam@gmail.com>
2022-04-06 05:38:47 -0400

    Grabing and styling more info

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let user ='octocat'
 let url = `https://api.github.com/users/octocat`;
 let user_icon = document.getElementById('user_icon');
 let user_name = document.getElementById('username');
-let user_AT_name = document.getElementById('user@name');
+let user_AT_name = document.getElementById('user_AT_name');
+let join = document.getElementById('birth');
 
 
 
@@ -12,6 +13,7 @@ fetch(url)
 user_icon.src= out.avatar_url;
 user_name.innerHTML = out.name;
 user_AT_name.innerHTML += out.login;
+join.innerHTML = out.created_at;
 console.log(out)
 })
 .catch(err => { throw err });
