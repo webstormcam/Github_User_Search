8bbbdffc9afafcbb29c7a2d1e67afe255d82a67f
Cameron Chandler <webstormcam@gmail.com>
2022-04-06 06:12:54 -0400

    Made the date information appear correctly

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let user_icon = document.getElementById('user_icon');
 let user_name = document.getElementById('username');
 let user_AT_name = document.getElementById('user_AT_name');
 let join = document.getElementById('birth');
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 
 
@@ -13,7 +14,13 @@ fetch(url)
 user_icon.src= out.avatar_url;
 user_name.innerHTML = out.name;
 user_AT_name.innerHTML += out.login;
-join.innerHTML = out.created_at;
+let dateObj = new Date(out.created_at);
+let month = dateObj.getUTCMonth(); 
+let day = dateObj.getUTCDate();
+let year = dateObj.getUTCFullYear();
+let month_name = months[month]
+let newDate = ` Joined ${day} ${month_name} ${year}`;
+join.innerHTML = newDate
 console.log(out)
 })
 .catch(err => { throw err });
