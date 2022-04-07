605540f754768c678ab0ee9f0581d3a2e3fe7c44
Cameron Chandler <webstormcam@gmail.com>
2022-04-07 05:57:57 -0400

    Made the user search kind of work

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let user_name = document.getElementById('username');
 let user_AT_name = document.getElementById('user_AT_name');
 let join = document.getElementById('birth');
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let user_story = document.getElementById('userstory');
+let user_search = document.getElementById('user_search');
+let user_button = document.getElementById('user_button');
 
 
-
+function runSearch(){
 fetch(url)
 .then(res => res.json())
 .then((out) => {
 user_icon.src= out.avatar_url;
 user_name.innerHTML = out.name;
-user_AT_name.innerHTML += out.login;
+user_AT_name.innerHTML = `@${out.login}`;
 let dateObj = new Date(out.created_at);
 let month = dateObj.getUTCMonth(); 
 let day = dateObj.getUTCDate();
@@ -21,9 +24,25 @@ let year = dateObj.getUTCFullYear();
 let month_name = months[month]
 let newDate = ` Joined ${day} ${month_name} ${year}`;
 join.innerHTML = newDate
+if(out.bio === null){
+user_story.innerHTML ='This profile has no bio'
+} else{
+    user_story.innerHTML = out.bio;
+}
 console.log(out)
 })
 .catch(err => { throw err });
+}
+user_button.addEventListener('click',function(){
+    user_search = user_search.value;
+    url = `https://api.github.com/users/${user_search}`
+    runSearch();
+})
+
+
+runSearch();
+
+
 
 
 
