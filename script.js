19b28a9caf1c3aeffdb9749a73266cb1f2eb466b
Cameron Chandler <webstormcam@gmail.com>
2022-04-11 06:18:30 -0400

    Got the hover effects to work correclty

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ let mode_word = document.getElementById('mode_word')
 let mode_img = document.getElementById('mode_img')
 let body = document.getElementById('holder');
 let logo= document.getElementById('logo');
+let user_info = document.getElementById('user_information')
 
 
 function runSearch(){
@@ -133,36 +134,38 @@ if(mode_word.innerHTML ==='DARK'){
     mode_img.src = 'imgs/icon-sun.svg'
     logo.style.color ='#fff'
     body.style.backgroundColor='#141D2F'
+    user_info.style.backgroundColor='#1E2A47'
 } else if(mode_word.innerHTML==='LIGHT'){
     mode_word.innerHTML='DARK'
     mode_img.src = 'imgs/icon-moon.svg'
     mode_word.style.color ='#4B6A9B'
     logo.style.color='black'
+    user_info.style.backgroundColor='#fff'
     body.style.backgroundColor='#fff'
 }
 });
 
 
-// mode_switch.addEventListener('mouseover',function(){
-//     if(mode_word.innerHTML==='DARK'){
-//         mode_img.src ='imgs/icon-moon - Copy.svg'
-//         mode_word.style.color = "#222731"
-//     } else if(mode_word.innerHTML==='LIGHT'){
-//         mode_img.src ='imgs/icon-sun - Copy.svg'
-//         mode_word.style.color ="#90A4D4"
-//     }
-// })
+mode_switch.addEventListener('mouseover',function(){
+    if(mode_word.innerHTML==='DARK'){
+        mode_img.src ='imgs/icon-moon - Copy.svg'
+        mode_word.style.color = "#222731"
+    } else if(mode_word.innerHTML==='LIGHT'){
+        mode_img.src ='imgs/icon-sun - Copy.svg'
+        mode_word.style.color ="#90A4D4"
+    }
+})
 
 
-// mode_switch.addEventListener('mouseout',function(){
-//     if(mode_word.innerHTML==='DARK'){
-//         mode_img.src ='imgs/icon-moon.svg'
-//         mode_word.style.color ='#4B6A9B'
-//     } else if(mode_word.innerHTML==='LIGHT'){
-//         mode_img.src ='imgs/icon-sun.svg'
-//         mode_word.style.color ="#fff"
-//     }
-// })
+mode_switch.addEventListener('mouseout',function(){
+    if(mode_word.innerHTML==='DARK'){
+        mode_img.src ='imgs/icon-moon.svg'
+        mode_word.style.color ='#4B6A9B'
+    } else if(mode_word.innerHTML==='LIGHT'){
+        mode_img.src ='imgs/icon-sun.svg'
+        mode_word.style.color ="#fff"
+    }
+})
 
 
 
